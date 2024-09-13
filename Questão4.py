49ff1b324d5d5a7159d321447b074224433b5f1b
Igor Lobato <igor_stm@yahoo.com.br>
2024-09-13 01:23:33 -0300

    Questão 4 concluída!

---
+++ Questão4.py
@@ -0,0 +1,29 @@
+'''4) Descubra a lógica e complete o próximo elemento:
+a) 1, 3, 5, 7, ___
+b) 2, 4, 8, 16, 32, 64, ____
+c) 0, 1, 4, 9, 16, 25, 36, ____
+d) 4, 16, 36, 64, ____
+e) 1, 1, 2, 3, 5, 8, ____
+f) 2,10, 12, 16, 17, 18, 19, ____'''
+
+'''Devido a natureza do problema achei melhor resolver o problema de forma lógica e sem programar
+
+a) 1, 3, 5, 7, ___
+Sequência de números ímpares. Próximo número é 9!
+
+b) 2, 4, 8, 16, 32, 64, ____
+Cada número é o dobro do anterior. Próximo número é 128!
+
+c) 0, 1, 4, 9, 16, 25, 36, ____
+Sequência dos quadrados (0², 1², 2³, 3², ...). Próximo número é 49!
+
+d) 4, 16, 36, 64, ____
+Assim como o anterior, esse é uma sequência dos quadrados, mas considerando apenas os números pares, Próximo número é 100!
+
+e) 1, 1, 2, 3, 5, 8, ____
+É uma sequência de fibonacci. Próximo número é 13!
+
+f) 2, 10, 12, 16, 17, 18, 19, ____
+Infelizmente não consegui encontrar a lógica nessa sequência,o próximo número poderia ser 20 caso os números contiassem a somar +1
+como os últimos 4. Mas provavelmente estou errado.
+'''
